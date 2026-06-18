f282a052860c47d7f543869c2888426692366ff4
Hai Long <longdh2.dev@gmail.com>
2026-06-18 22:58:37 +0700

    feat: build ios

--- react-native.config.js
+++ react-native.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  assets: ['./node_modules/react-native-vector-icons/Fonts'],
-};
+// iOS fonts are bundled by the RNVectorIcons CocoaPod.
+// Android fonts are provided by @react-native-vector-icons/* packages.
+module.exports = {};
